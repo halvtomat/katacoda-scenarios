873e6f73cd4053b8e4c5f15447417999ff06fa76
Daniel <danne_1998@hotmail.se>
2022-04-28 10:35:44 +0200

    fix

--- github-action/setup.sh
+++ github-action/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-git config --global user.name "katacoda"
-git config --global user.email git-action-katacoda@mail.com
 git clone https://github.com/git-action-katacoda/github-action-katacoda.git
-#password: "Aaa12312."
+git-action-katacoda
+Aaa12312.
 cd github-action-katacoda
 rm -rf *
 touch README.md
