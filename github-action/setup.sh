5fef56f074729ae43b40debba96581a578947911
Daniel <danne_1998@hotmail.se>
2022-04-28 10:31:12 +0200

    fix bug

--- github-action/setup.sh
+++ github-action/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apt-get install git
 git clone https://github.com/git-action-katacoda/github-action-katacoda.git
 git config --global user.name "katacoda"
 git config --global user.email git-action-katacoda@mail.com
